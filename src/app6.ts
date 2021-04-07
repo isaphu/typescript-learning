dd26b79f891570630fc922644eebf4ab1c0a78f2
isaphu <uronohan@gmail.com>
2021-04-07 14:08:39 +0700

    adding new files

--- src/app6.ts
+++ src/app6.ts
@@ -1,1 +1,22 @@
+const userName = 'Isa';
+// userName = 'Isa Phuyuthanon'
+let age = 25;
+age = 30;
 
+function addTwoNum(a: number, b: number) {
+    let result;
+    result = a + b; 
+    return result;
+}
+
+console.log(addTwoNum(5,4))
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if(button) {
+    button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(addTwoNum(666,5))
