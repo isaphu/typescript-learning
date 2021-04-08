4604c70b136b2d3262e8fe359c633dcb68516442
isaphu <uronohan@gmail.com>
2021-04-08 14:05:23 +0700

    adding generics

---
+++ src/generics.ts
@@ -0,0 +1,12 @@
+const names = ['Isa', 'Jack'];
+
+/*
+
+generic type "Array<t>" is a type
+which is kind of connected with 
+some other types and its really flexible 
+regarding which exactly type that other type is. 
+
+*/
+
+const names: Array = [];
